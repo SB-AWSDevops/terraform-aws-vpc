53cbc8a09d61c757ebd085a5f12ce11b76c9071c
SurisettyGit <surisetty08@gmail.com>
2024-05-26 12:14:01 +0530

    vpc test module

--- vpc.tf
+++ vpc.tf
@@ -1,6 +1,6 @@
 module "vpc" {
 #  source = "../terraform-aws-vpc"
- source = "git::https://github.com/SB-AWSDevops/terraform-aws-vpc.git"
+ source = "git::https://github.com/SB-AWSDevops/DAWS-78/terraform-aws-vpc.git"
  project_name = var.project_name
  common_tags = var.common_tags
  public_subnet_cidrs = var.public_subnet_cidrs

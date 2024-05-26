19dd813915526875866fca58921d7b16b7af7859
SurisettyGit <surisetty08@gmail.com>
2024-05-26 15:05:49 +0530

    removed unneccesaray

--- output.tf
+++ output.tf
@@ -1,11 +1,31 @@
-output "subnet_info"{
-    value = module.vpc.subnet_info
+# output "azs" {
+#     value = data.aws_availability_zones.available.names
+# }
+
+output "vpc_id" {
+  value = aws_vpc.main.id
 }
 
-output "vpc_id"{
-    value = module.vpc.vpc_id
+output "public_subnet_ids" {
+  value = aws_subnet.public[*].id
 }
 
-output "database_subnet_group_id"{
-  value = module.vpc.database_subnet_group_id
+output "private_subnet_ids" {
+  value = aws_subnet.private[*].id
+}
+
+output "database_subnet_ids" {
+  value = aws_subnet.database[*].id
+}
+
+output "database_subnet_group_id" {
+  value = aws_db_subnet_group.default.id
+}
+
+output "database_subnet_group_name" {
+  value = aws_db_subnet_group.default.name
+}
+
+output "igw_id" {
+  value = aws_internet_gateway.gw.id
 }
